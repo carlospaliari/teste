aa4a093da64c689bf2b476e3780114eb78c3b465
Carlos Paliari <carlos@paliari.com.br>
2014-01-31 11:55:44 -0200

    include_date

---
+++ include_date.js
@@ -0,0 +1,42 @@
+    angular.module('plunker', ['ui.bootstrap']);
+var DatepickerDemoCtrl = function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+      $scope.toggleMin = function() {
+          $scope.minDate = ( $scope.minDate );
+      };
+
+  };
+  $scope.today();
+
+  $scope.showWeeks = true;
+  $scope.toggleWeeks = function () {
+    $scope.showWeeks = ! $scope.showWeeks;
+  };
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === null) );
+  };
+
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    'year-format': "'yy'",
+    'starting-day': 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+  $scope.format = $scope.formats[0];
+};
