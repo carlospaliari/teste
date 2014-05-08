15eb9f13617f29ca4f4621839fa8d78596a7ca83
carlospalairi <carlos@paliari.com.br>
2014-05-08 13:44:18 -0300

    acesso login funcionando

--- acesso_login/login_insere.php
+++ acesso_login/login_insere.php
@@ -1,7 +1,10 @@
 <?php
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$sql="INSERT INTO pessoa(email, password) VALUES ('$email', '$password')";
-$res= mysql_query($sql);
+echo '<pre>';
+var_export($_POST);
+echo '</pre>';
+exit;
+//$email = $_POST['email'];
+//$password = $_POST['password'];
+//
+//$sql="INSERT INTO pessoa(email, password) VALUES ('$email', '$password')";
+//$res= mysql_query($sql);
