ca48dd87e4df1a418d373fc40c8f4d9639a43aef
carlospalairi <carlos@paliari.com.br>
2014-05-22 15:23:51 -0300

    nova pagina apos o login

--- acesso_login/login_insere.php
+++ acesso_login/login_insere.php
@@ -3,8 +3,28 @@ echo '<pre>';
 var_export($_POST);
 echo '</pre>';
 exit;
+
+
 //$email = $_POST['email'];
 //$password = $_POST['password'];
 //
 //$sql="INSERT INTO pessoa(email, password) VALUES ('$email', '$password')";
 //$res= mysql_query($sql);
+?>
+
+<html>
+
+<head>
+    <title>Paliari</title>
+    <link rel="icon" type="image/png" href="icons.png">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+</body>
+</html>
